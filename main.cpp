f33301250ec1cf380ceebae1db9830e4f6549b1a
SpyguyOO7 <wgao2005007@gmail.com>
2023-07-18 11:40:56 -0400

    init_commit

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 
 int main()
 {
-  std::cout << "Hello, world!" << std::endl;
+  std::cout << "William Gao, AUV" << std::endl;
 
   int x = 5;
   int y = 7;
+  int z = 15;
+  int v = 8;
+  double divResult = (double)(x + y )/ (double)v;
+
   std::cout << "The sum of " << x << " and " << y << " is " << x + y << std::endl;
+  std::cout << "The sum of " << x << ", " << y << ", and "<< z <<" is " << x + y + z << std::endl;
+  std::cout << "The sum of " << x << " and " << y << " divided by "<< v <<" is " <<   divResult<< std::endl;
 
   return 0;
 }
